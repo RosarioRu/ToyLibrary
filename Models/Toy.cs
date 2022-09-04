748da6b6d14409e1c9753ee9b5b8e12893042779
RosarioRu <mariarruval@gmail.com>
2022-09-04 16:20:54 -0700

    Add Toy class in the Models directory.

---
+++ Models/Toy.cs
@@ -0,0 +1,13 @@
+namespace ToyLibrary.Models
+{
+  public class ToyLibrary
+  {
+    public int ToyId {get; set;}
+
+    public string Name {get; set;}
+
+    public string Description {get; set;}
+
+    public int TypicalUserAge {get; set;}
+  }
+}
