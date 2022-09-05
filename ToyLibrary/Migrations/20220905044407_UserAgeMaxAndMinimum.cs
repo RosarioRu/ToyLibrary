65f7a52f3c33aa84ec57882af3e6e9132a399c4d
RosarioRu <mariarruval@gmail.com>
2022-09-04 21:45:49 -0700

    Complete second migration to update database tables to include user max and min age recommendations.

---
+++ ToyLibrary/Migrations/20220905044407_UserAgeMaxAndMinimum.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToyLibrary.Migrations
+{
+    public partial class UserAgeMaxAndMinimum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TypicalUserAge",
+                table: "Toys",
+                newName: "UserAgeMinimum");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserAgeMaximum",
+                table: "Toys",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserAgeMaximum",
+                table: "Toys");
+
+            migrationBuilder.RenameColumn(
+                name: "UserAgeMinimum",
+                table: "Toys",
+                newName: "TypicalUserAge");
+        }
+    }
+}
