018588e4036544892df395d6fb6631f4ce9ff1a1
RosarioRu <mariarruval@gmail.com>
2022-09-04 17:06:19 -0700

    Add toyscontroller file with methods to return all toys in db and create a toy.

---
+++ ToyLibrary/Controllers/ToysController.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToyLibrary.Models;
+
+namespace ToyLibrary.Controllers
+{
+  [route("api/[controller]")]
+  [ApiController]
+  public class ToysController: ControllerBase
+  {
+
+    private readonly ToyLibraryContext _db;
+
+    public ToysController(ToyLibraryContext db)
+    {
+      _db = db;
+    }
+
+    //GET api/toys
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Toy>>> Get()
+    {
+      return await _db.Toys.ToListAsynch();
+    }
+
+    //POST api/toys
+    [HttpPost]
+    public async Task<ActionResult<Toy>> Post (Toy toy)
+    {
+      _db.Toys.Add(toy);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction("Post", new {id = toy.ToyId}, toy);
+    }
+
+  }
+}
+
