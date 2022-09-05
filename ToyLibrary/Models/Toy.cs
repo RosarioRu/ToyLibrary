164cda2a9d97c15b5246d6b02ee963c49942f6d4
RosarioRu <mariarruval@gmail.com>
2022-09-04 21:52:30 -0700

    Add validation and requirement and range attributes for some of the Toy properties.

--- ToyLibrary/Models/Toy.cs
+++ ToyLibrary/Models/Toy.cs
@@ -1,15 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ToyLibrary.Models
 {
   public class Toy
   {
     public int ToyId {get; set;}
 
+    [Required]
+    [StringLength(20)]
     public string Name {get; set;}
 
+    [Required]
     public string Description {get; set;}
 
+    [Required]
+    [Range(0,17, ErrorMessage = "Minimum age must be between 0 and 17.")]
     public int UserAgeMinimum {get; set;}
 
+    [Required]
+    [Range(0,17, ErrorMessage = "Maximum age must be between 0 and 17.")]
     public int UserAgeMaximum {get; set;}
   }
 }
